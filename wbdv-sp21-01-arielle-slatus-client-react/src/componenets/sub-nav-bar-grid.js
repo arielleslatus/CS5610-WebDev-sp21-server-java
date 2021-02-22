4ec8ac59c7aecd9f3535b9df775964ec6061c176
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-02-22 13:25:10 -0500

    added icons to grid

--- wbdv-sp21-01-arielle-slatus-client-react/src/componenets/sub-nav-bar-grid.js
+++ wbdv-sp21-01-arielle-slatus-client-react/src/componenets/sub-nav-bar-grid.js
@@ -9,6 +9,8 @@ const SubNavBarGrid = () =>
             <th className="col-xs-11 col-sm-11 col-md-9">Recent Documents</th>
             <th className="col-2 d-none d-md-block">Owned by me</th>
             <th className="col-xs-1 col-sm-1">
+                <i class="fas fa-sort-alpha-up-alt ats-icon-spacing"></i>
+                <i class="fas fa-folder ats-icon-spacing"></i>
                 <Link to="/courses/table">
                     <i className="fas fa-list float-right"></i>
 
